82740b8e2c8a56cd17dacedf5f4557907fce340a
Yurii <j.koroliak@gmail.com>
2025-05-05 20:00:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//1) Ask for the user's name and print “Hello, *name*”;
+let username = promt('what is ur name?');
+alert(username);
+
+
+
+
+
+
+
+
+
+
+//2)Ask the user for his/her year of birth, calculate his/her age and print the result. Use the current year as a constant in the code;
+//3)Ask the user the length of a side of a square and print the perimeter of the square
+
+
+
