3289c147c324e58cacf96749b157b5a0915f9b72
Yurii <j.koroliak@gmail.com>
2025-05-05 20:16:14 +0200

    first lesson tasks

--- main.js
+++ main.js
@@ -5,20 +5,25 @@ let b = 24;
 console.log (a+b)
 
 let username = prompt('what is ur name?');
-console.log(username)
-
-
-
-
-
-
-
+console.log('hello ' + username)
 
 
 
 
 //2)Ask the user for his/her year of birth, calculate his/her age and print the result. Use the current year as a constant in the code;
+
+const year = 2025;
+let userBirthYear = prompt(`what is ur year of birth?`);
+let userAge = year - userBirthYear;
+console.log(userAge);
+
+
+
 //3)Ask the user the length of a side of a square and print the perimeter of the square
+
+let sideLength = prompt('what is the length of a side of a square?');
+let squarePerimeter = sideLength * 4;
+console.log(squarePerimeter);
 
 
 
