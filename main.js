51534885123e479f06da71da07532a46ecbe316c
Yurii <j.koroliak@gmail.com>
2025-05-06 21:30:56 +0200

    second question, second task

--- main.js
+++ main.js
@@ -30,15 +30,21 @@
 
 
 //1.Ask the user for the radius of a circle and print the area of such a circle.
-let circleRadius = Number(prompt('what is radius of the circle?'));
-let circlePerimetr = Number((Math.PI*(Math.pow(circleRadius, 2))).toFixed(2));
-console.log(circlePerimetr);
+// let circleRadius = Number(prompt('what is radius of the circle?'));
+// let circlePerimetr = Number((Math.PI*(Math.pow(circleRadius, 2))).toFixed(2));
+// console.log(circlePerimetr);
 // Math.PI = число пи
 // toFixed(2) = округляет до 2 знаков после запятой
 // Math.pow = возводит в степень
 
 
 //2.Ask the user for the distance in kilometers between two cities and how many hours they want to get there. Calculate the speed at which you need to move to be on time.
+let distance = prompt('what is the distance between the two cities');
+let hours = prompt('how many hours do you want to get there?');
+let result = distance/hours;
+console.log(result + 'km/h');
+
+
 //3.Implement a currency converter. The user enters dollars, the program converts them into euros. The exchange rate is kept constant.
 
 
