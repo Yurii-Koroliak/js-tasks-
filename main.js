f36756a135c4bb629b496ac1f15890d13727d679
Yurii <j.koroliak@gmail.com>
2025-05-07 21:28:57 +0200

    first task second lesson

--- main.js
+++ main.js
@@ -30,20 +30,62 @@
 
 
 //1.Ask the user for the radius of a circle and print the area of such a circle.
-let circleRadius = Number(prompt('what is radius of the circle?'));
-let circlePerimetr = Number((Math.PI*(Math.pow(circleRadius, 2))).toFixed(2));
-console.log(circlePerimetr);
+// let circleRadius = Number(prompt('what is radius of the circle?'));
+// let circlePerimetr = Number((Math.PI*(Math.pow(circleRadius, 2))).toFixed(2));
+// console.log(circlePerimetr);
 // Math.PI = число пи
 // toFixed(2) = округляет до 2 знаков после запятой
 // Math.pow = возводит в степень
 
 
 //2.Ask the user for the distance in kilometers between two cities and how many hours they want to get there. Calculate the speed at which you need to move to be on time.
-let distance = prompt('what is the distance between the two cities');
-let hours = prompt('how many hours do you want to get there?');
-let result = distance/hours;
-console.log(result + 'km/h');
+// let distance = prompt('what is the distance between the two cities');
+// let hours = prompt('how many hours do you want to get there?');
+// let result = distance/hours;
+// console.log(result + 'km/h');
 
 
 
 
+
+
+
+
+
+
+
+// it was tasks for the first lesson
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TASKS FOR THE SECOND LESSON
+
+
+// Виконай додавання 0,1 і 0,2 добийся математично правильної відповіді.
+// Add 0.1 and 0.2 to get a mathematically correct answer.
+
+let a = 0.1;
+let b = 0.2;
+console.log((a+b).toFixed(1));
+
+
+
+// Виконай додавання рядка "1" і цифри 2 (обидві операнди повинні бути в змінних), добийся математично правильної відповіді.
+// Add the string “1” and the digit 2 (both operands must be in variables), get a mathematically correct answer.
+
+
+
+// Користувач вказує обсяг флешки в Гб. Програма повинна порахувати скільки файлів розміром в 820 Мб поміщається на флешку.
+// The user specifies the size of the flash drive in GB. The program should calculate how many files of size 820 MB can be placed on the flash drive.
