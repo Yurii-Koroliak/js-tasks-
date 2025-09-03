9fddd5ea2d2eb3f941464a364e0c5133751be8f7
Yurii <j.koroliak@gmail.com>
2025-09-03 22:36:14 +0200

    pillarsDistance

--- main.js
+++ main.js
@@ -939,3 +939,31 @@ function expressionMatter(a, b, c) {
   return result[result.length - 1];
   
 }
+
+
+
+
+
+// There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+// Рядом с дорогой стоят столбы. Расстояние между столбами одинаковое, ширина столбов одинаковая. Ваша функция принимает три аргумента:
+// количество столбов (≥ 1);
+// расстояние между столбами (10 - 30 метров);
+// ширина столба (10 - 50 сантиметров).
+// Вычислите расстояние между первым и последним столбом в сантиметрах (без учета ширины первого и последнего столба).
+
+
+function pillars(numPill, dist, width) {
+  // your code here
+  let betweenDist = dist*100;
+  let distance;
+  if (numPill <= 1) {
+    return 0;
+  } else {  
+      distance = (betweenDist*(numPill-1))+(width*(numPill-2));
+  }
+  return distance;
+}
